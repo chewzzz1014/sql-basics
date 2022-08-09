09f40ec9e9d4ea639fb0ff4533acd0a19a68da26
chewzzz1014 <92832451+chewzzz1014@users.noreply.github.com>
2022-08-09 13:36:27 +0800

    Update practice_join.sql

--- notes/udacity/practice_join.sql
+++ notes/udacity/practice_join.sql
@@ -13,3 +13,33 @@ SELECT orders.standard_qty, orders.gloss_qty, orders.poster_qty,
 FROM orders
 JOIN accountss
     ON orders.id = accounts.id;
+
+
+-- Solution for : https://learn.udacity.com/courses/ud198/lessons/279f3b98-a2ca-46ef-975b-e943daffc30b/concepts/e80fffa3-0f87-405e-8675-2d23a4dd51ce
+
+-- Q1
+SELECT a.primary_poc, w.occurred_at, w.channel, a.name
+FROM accounts a
+JOIN web_events w
+    ON a.id = w.id
+WHERE a.name = 'Walmart';
+
+-- Q2
+SELECT r.name, s.name, a.name
+FROM region r
+JOIN sales_reps s
+    ON s.region_id = r.id
+JOIN accounts a
+    ON s.id = a.sales_rep_id
+ORDER BY a.name;
+    
+-- Q3
+SELECT r.name region, a.name account, (o.total_amt_usd / (o.total + 0.01)) unit_price
+FROM region r
+JOIN sales_reps s
+     ON r.id = s.region_id
+JOIN accounts a
+     ON s.id = a.sales_rep_id
+JOIN orders o
+     ON o.account_id = a.id;
+     
