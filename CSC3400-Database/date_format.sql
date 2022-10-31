26e5eb22a1d8e67a09ebff7121693e67098b6d17
chewzzz <chewziqing@gmail.com>
2022-10-31 12:24:21 +0800

    Add formatting date

--- CSC3400-Database/date_format.sql
+++ CSC3400-Database/date_format.sql
@@ -3,4 +3,28 @@ SELECT
     value
 FROM V$NLS_PARAMETERS
 WHERE
-    parameter = 'NLS_DATE_FORMAT';
+    parameter = 'NLS_DATE_FORMAT';  -- DD/MM/RRRR
+    
+    
+    
+-- return current date
+SELECT sysdate
+FROM dual;  -- 31/20/2022
+
+
+-- change date format on sql developer
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+
+
+
+-- convert date to string
+-- October 31, 2022
+SELECT
+    TO_CHAR(SYSDATE, 'FMMonth DD, YYYY')
+FROM 
+    dual;
+    
+SELECT
+    TO_CHAR(SYSDATE, 'DD/FMMonth/YYYY')
+FROM 
+    dual;
