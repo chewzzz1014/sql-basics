c8007c7696cc88df8493ce2ca9193bc3b24a601f
chewzzz <chewziqing@gmail.com>
2022-10-31 12:42:37 +0800

    Add q1-13

--- CSC3400-Database/lab4.sql
+++ CSC3400-Database/lab4.sql
@@ -5,5 +5,15 @@ SELECT
     LENGTH(last_name) as last_name_length,
     INSTR(last_name, 'a') as position_of_n
 FROM employees
-WHERE last_name LIKE '%n%';
+WHERE last_name LIKE '%n';
+
+
+-- Q2
+SELECT
+    sysdate AS "DATE"
+FROM dual;
     
+-- Q3: num of weeks empoyed
+SELECT last_name, EXTRACT(YEAR FROM sysdate) - EXTRACT(YEAR FROM hire_date)
+FROM employees
+WHERE department_id = 60;
