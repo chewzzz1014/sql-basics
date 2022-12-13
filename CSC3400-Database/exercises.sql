1e8804d16b187373a5e94a929cc2f8d1c2683de4
chewzzz <chewziqing@gmail.com>
2022-12-13 09:17:00 +0800

    Add q2

--- CSC3400-Database/exercises.sql
+++ CSC3400-Database/exercises.sql
@@ -1,8 +1,18 @@
+-- 1
 SELECT
  first_name || ' ' || last_name "Employee Name",
- 
  -- Use "" to reserve the small case of column name
  email "Email", 
  phone_number "Employee-Phone"
 FROM employees
 ORDER BY first_name;
+
+-- 2
+SELECT 
+    department_name "Department Name",
+    j.department_id "No of Department"
+FROM job_history j
+JOIN departments d
+    ON j.department_id = d.department_id
+GROUP BY department_name, j.department_id
+HAVING COUNT(*)>1;
