c0086ba96d18b92cdf861859df1865cf1a6c5c74
chewzzz <chewziqing@gmail.com>
2022-12-13 08:51:22 +0800

    Add comment

--- CSC3400-Database/exercises.sql
+++ CSC3400-Database/exercises.sql
@@ -1,6 +1,8 @@
 SELECT
  first_name || ' ' || last_name "Employee Name",
- email "Email",
+ 
+ -- Use "" to reserve the small case of column name
+ email "Email", 
  phone_number "Employee-Phone"
 FROM employees
 ORDER BY first_name;
