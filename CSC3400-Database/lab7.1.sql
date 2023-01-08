dd1579c7128035a6cec641beaacdaa8a7a868e55
chewzzz <chewziqing@gmail.com>
2023-01-08 15:07:07 +0800

    Add sequences

--- CSC3400-Database/lab7.1.sql
+++ CSC3400-Database/lab7.1.sql
@@ -41,3 +41,35 @@ WITH READ ONLY;
 -- Q8
 INSERT INTO v2customer
     VALUES('C00007','Roslina Ahmad','0198888999');
+    
+-- Q9
+CREATE SEQUENCE CustomerIDSeq
+INCREMENT BY 1
+START WITH 10
+MAXVALUE 99999
+NOCACHE
+NOCYCLE;
+
+-- Q10
+SELECT sequence_name, min_value, max_value,
+increment_by, last_number
+from user_sequences;
+
+-- Q11
+SELECT custid FROM CUSTOMER;
+
+-- Q12
+INSERT INTO customer
+    VALUES ('C'||LPAD(CustomerIDSeq.NEXTVAL, 5, 0), 'Arizawati Saad', '0132119901', 'P', 'S00002', null);
+    
+SELECT custid FROM customer WHERE custname='Arizawati Saad';
+
+
+-- Q13
+Select sequence_name, min_value, max_value, 
+increment_by, last_number 
+from user_sequences
+where sequence_name = 'CUSTOMERIDSEQ';
+
+-- Q14
+DROP SEQUENCE CustomerIDSeq;
