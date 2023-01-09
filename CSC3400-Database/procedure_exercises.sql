8beefd367805791c75e07efaaa0917c6129c41e4
chewzzz <chewziqing@gmail.com>
2023-01-09 11:53:27 +0800

    Add function

--- CSC3400-Database/procedure_exercises.sql
+++ CSC3400-Database/procedure_exercises.sql
@@ -13,3 +13,18 @@ BEGIN
 Greetings:= 'Hello World';
 dbms_output.put_line (greetings);
 END hello;
+
+-- create function
+CREATE OR REPLACE FUNCTION totalCustomers
+RETURN NUMBER
+IS
+    total NUMBER(2) := 0;
+BEGIN
+    -- select total numbers of customers
+    SELECT COUNT(*)
+    INTO total
+    FROM customers;
+    
+    -- return the total sales
+    RETURN total;
+END;
