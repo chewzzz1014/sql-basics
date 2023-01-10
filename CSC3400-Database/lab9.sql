8ee8774fa1c521a79dd0fb35b718f37859fcfd17
chewzzz <chewziqing@gmail.com>
2023-01-10 09:16:02 +0800

    Done q1

--- CSC3400-Database/lab9.sql
+++ CSC3400-Database/lab9.sql
@@ -3,14 +3,16 @@ CREATE OR REPLACE FUNCTION add_two_value (n1 number, n2 number)
 RETURN NUMBER
 IS 
     result Number(2) := 0;
-BEGIN 
+BEGIN
     result := n1 + n2;
     RETURN result;
 END;
 
 DECLARE
     ans NUMBER(2):= 0;
+    a NUMBER(2) := 2;
+    b NUMBER(2) := 5;
 BEGIN
-    ans := add_two_value(2, 5);
+    ans := add_two_value(a, b);
     DBMS_OUTPUT.PUT_LINE('Total of 2 and 5 is: ' || ans);
 END;
