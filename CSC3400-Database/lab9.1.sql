e27598ef5f6b666daa2a48626a7800e5a6c3f1a5
chewzzz <chewziqing@gmail.com>
2023-01-14 20:19:29 +0800

    Complete q2

--- CSC3400-Database/lab9.1.sql
+++ CSC3400-Database/lab9.1.sql
@@ -17,25 +17,56 @@ CREATE TABLE student_gpa(
     total_credit NUMBER
 );
 
-CREATE OR REPLACE PROCEDURE calculate_gpa(input_matric NUMBER, session VARCHAR)
-IS
-    temp_table registration%type;
+-- DROP TABLE student_gpa;
+
+CREATE OR REPLACE PROCEDURE calculate_gpa
+AS
     total_credit NUMBER := 0;
     total_grade_point NUMBER := 0;
     gpa NUMBER := 0;
+    CURSOR student_info
+    IS
+        SELECT 
+            DISTINCT matric, sem_session, 
+            SUM(credit) as t_credit
+        FROM registration r1
+        GROUP BY matric, sem_session
+        ORDER BY matric;
+    CURSOR all_info 
+    IS
+        SELECT * FROM registration;
 BEGIN
-    SELECT SUM(credit) INTO total_credit
-    FROM registration
-    WHERE matric = input_matric AND sem_session = session;
-    
-    
+    FOR s1 IN student_info
+    LOOP
+        total_credit := s1.t_credit;
+        total_grade_point := 0;
+        FOR s2 IN all_info
+        LOOP
+            IF(s2.matric = s1.matric AND s2.sem_session = s1.sem_session)
+            THEN
+                CASE s2.grade
+                    WHEN 'A' THEN total_grade_point := total_grade_point + (4*s2.credit);
+                    WHEN 'A-' THEN total_grade_point := total_grade_point + 3.75*s2.credit;
+                    WHEN 'B+' THEN total_grade_point := total_grade_point + 3.5*s2.credit;
+                    WHEN 'B' THEN total_grade_point := total_grade_point + 3*s2.credit;
+                    WHEN 'B-' THEN total_grade_point := total_grade_point + 2.75*s2.credit;
+                    WHEN 'C+' THEN total_grade_point := total_grade_point + 2.5*s2.credit;
+                    WHEN 'C' THEN total_grade_point := total_grade_point + 2*s2.credit;
+                    WHEN 'C-' THEN total_grade_point := total_grade_point + 1.75*s2.credit;
+                    WHEN 'D+' THEN total_grade_point := total_grade_point + 1.5*s2.credit;
+                    WHEN 'D' THEN total_grade_point := total_grade_point + 1*s2.credit;
+                    ELSE total_grade_point := total_grade_point + 0*s2.credit;
+                END CASE;
+            END IF;
+        END LOOP;
+            gpa := total_grade_point / total_credit;
+            INSERT INTO student_gpa VALUES (s1.matric, s1.sem_session, ROUND(gpa, 2), total_credit);
+    END LOOP;
     dbms_output.put_line (total_credit);
 END;
     
 BEGIN
-    calculate_gpa(214321, 'SEM 1 2021-2022');
+    calculate_gpa();
 END;
---SELECT DISTINCT matric, sem_session, SUM(credit)
---FROM registration
---GROUP BY matric, sem_session
---ORDER BY matric;
+
+SELECT * FROM student_gpa;
