7cc72a9d5dfc51ce3370db6f8dc8c76394f2272a
chewzzz <chewziqing@gmail.com>
2023-01-14 19:46:17 +0800

    Add q2

--- CSC3400-Database/lab9.1.sql
+++ CSC3400-Database/lab9.1.sql
@@ -8,3 +8,34 @@ AS
         r.grade = g.grade;
 
 SELECT * FROM REGISTRATION_GRADE_POINT;
+
+-- Q2
+CREATE TABLE student_gpa(
+    matric NUMBER,
+    sem_session VARCHAR2(20),
+    gpa NUMBER(4, 2),
+    total_credit NUMBER
+);
+
+CREATE OR REPLACE PROCEDURE calculate_gpa(input_matric NUMBER, session VARCHAR)
+IS
+    temp_table registration%type;
+    total_credit NUMBER := 0;
+    total_grade_point NUMBER := 0;
+    gpa NUMBER := 0;
+BEGIN
+    SELECT SUM(credit) INTO total_credit
+    FROM registration
+    WHERE matric = input_matric AND sem_session = session;
+    
+    
+    dbms_output.put_line (total_credit);
+END;
+    
+BEGIN
+    calculate_gpa(214321, 'SEM 1 2021-2022');
+END;
+--SELECT DISTINCT matric, sem_session, SUM(credit)
+--FROM registration
+--GROUP BY matric, sem_session
+--ORDER BY matric;
